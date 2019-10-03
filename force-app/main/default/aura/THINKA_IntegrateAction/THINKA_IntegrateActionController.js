87ea2d87abe7ebcdf90153531a588bba099164e3
Yosuke Sakamura <y-sakamura@digital-sphere.co.jp>
2019-10-03 09:11:36 +0900

    Add ltng quick action

--- force-app/main/default/aura/THINKA_IntegrateAction/THINKA_IntegrateActionController.js
+++ force-app/main/default/aura/THINKA_IntegrateAction/THINKA_IntegrateActionController.js
@@ -1,12 +1,5 @@
 ({
     exec : function(c, e, h) {
-        var action = c.get('c.exec');
-        action.setParams({
-            recordId: c.get('v.recordId')
-        });
-        action.setCallback(this, function(result) {
-
-        });
-        $A.enqueueAction(action);
+        h.exec(c);
     }
 })
--- force-app/main/default/aura/THINKA_IntegrateAction/THINKA_IntegrateActionHelper.js
+++ force-app/main/default/aura/THINKA_IntegrateAction/THINKA_IntegrateActionHelper.js
@@ -1,5 +1,44 @@
 ({
-    helperMethod : function() {
+    exec : function(c) {
+        // Apex
+        const action = c.get('c.registerOnThinka');
+        action.setParams({
+            recordId: c.get('v.recordId'),
+        });
+        // Callback
+        action.setCallback(this, function(res) {
+            const state = res.getState();
+            const toastEvent = $A.get("e.force:showToast");
 
+            // Window Close
+            $A.get("e.force:closeQuickAction").fire();
+
+            // Success ->
+            if (state === 'SUCCESS') {
+                toastEvent.setParams({
+                    title : 'Success!!',
+                    message:'Cancel processing completed.',
+                    messageTemplate: '',
+                    duration:' 5000',
+                    key: 'info_alt',
+                    type: 'success',
+                    mode: 'pester'
+                });
+            }
+            // Error ->
+            else {
+                toastEvent.setParams({
+                    title : 'Error!!',
+                    message:'Cancel processing failed.',
+                    messageTemplate: '',
+                    duration:' 5000',
+                    key: 'info_alt',
+                    type: 'error',
+                    mode: 'pester'
+                });
+            }
+            toastEvent.fire();
+        })
+        $A.enqueueAction(action);
     }
 })
